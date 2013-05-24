ba16d8c87efe69141fc2fc6992a7818d2b986dcb
Alex M (coyo) <coyo@darkdna.net>
2013-05-23 19:26:55 -0500

    Create lupine.py
    also Moon Moon.

---
+++ src/lupine/lupine.py
@@ -0,0 +1,13 @@
+#!/env/python
+
+# an automated autoconfiguration script that acts as an independent library from fennec.
+
+# this script will eventually be packaged as a seperate project as a network-negotiation
+# library that other projects can use for orchestrated upnp, pwnat, ice and many other methods
+# that can be trivially used to provided nearly-guaranteed capacity to accept incoming connections 
+# from any software that also uses lupine.
+
+# named after wolves because lupine attempts to encourage the solidarity of a wolf pack between
+# services exposed by way of lupine. In addition, wolves are awesome.
+
+echo 'Wolves are cool, except for Moon Moon. Who invited him, anyway?'
