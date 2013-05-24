faab20b95da5bc73adc3e1918a0d0ddccaba0b24
Alex M (coyo) <coyo@darkdna.net>
2013-05-23 19:47:50 -0500

    Create INSTALL.py
    the scripts! they do nothing!

---
+++ INSTALL.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python
+
+#@Author: Alex J. Maurin (AKA Coyo)
+#@Date: Thu May 23 19:44:47 2013 CST (GMT -6)
+
+# I am not a very competent python coder, so please forgive my horrifying code.
+# I just wanted to go ahead and get started with something.
+# INSTALL.py does exactly what it says on the tin, intended to automate
+# the configuration, compilation and installation of the Fennec Overlay.
+
+echo 'The scripts! They do nothing!'
