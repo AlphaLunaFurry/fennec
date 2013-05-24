aad9057c90a23676c9d1cbdb44cd4b611a619384
Alex M (coyo) <coyo@darkdna.net>
2013-05-23 19:21:11 -0500

    Update and rename src/tor/torrc.conf to src/setup.py

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+#!/env/python
+
+# this script will automagically orchestrate all preparations, sanity checks, and autoconfigurations
+# during automated compilation, including checking for dependancies and querying the user
+# for important configuration options if not provided with flags or an unattended configuration
+# preconfigure.conf file.
+
+echo 'herpa durp..'
